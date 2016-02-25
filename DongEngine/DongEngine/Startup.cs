97e5b319f7a9748de9b0fab0a18725e5219dbfa6
Fourslash <fourslash@gmail.com>
2016-02-26 00:52:23 +0300

    init

---
+++ DongEngine/DongEngine/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codeplex.Data;
+
+namespace DongEngine
+{
+    public class Startup
+    {
+        public async Task<object> Invoke(dynamic obj)
+        {
+            return "ds3";
+        }
+    }
+}
---
+++ DongJS/DongJS/app.js
@@ -0,0 +1,22 @@
+﻿var express = require('express');
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var edge = require('edge');
+var Promise = require('bluebird');
+var app = express();
+
+var sharpFunc = edge.func('../../DongEngine/DongEngine/bin/Debug/DongEngine.dll');
+
+var doStuff = async(function () {
+	var sharpPromise = Promise.promisify(sharpFunc);
+	return await(sharpPromise({ hi: 1, test: 2 }));
+});
+
+app.get('/', async(function (req, res) {
+	var result = await(doStuff());
+	res.send({ result: result });
+}));
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
